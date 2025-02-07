d345bc458ec8855158b09d77b93f107d49f82037
gustavohipolito <128245536+gustavohipolito@users.noreply.github.com>
2025-02-07 17:30:40 -0300

    Add files via upload

---
+++ en (1).php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array(
+		"warning"            => "O firewall neste servidor está bloqueando sua conexão.",
+		"contact"            => "Entre em contato com o suporte caso o seu IP continue a ser bloqueado.",
+		"blocked ip"         => "Seu IP bloqueado é:",
+		"hostname"           => "O nome deste servidor é:",
+		"recaptcha title"    => "Você pode desbloquear o seu IP utilizando o ReCAPTCHA:",
+		"recaptcha note"     => "Nota: Nem todas as solicitações de desbloqueio serão bem-sucedidas, pois depende de como seu endereço IP está sendo bloqueado. Se o desbloqueio falhar, você precisará entrar em contato com o suporte para obter mais informações.",
+		"recaptcha success"  => "ReCAPTCHA concluído. Favor aguardar alguns segundos que o seu IP será desbloqueado:",
+		"recaptcha failure"  => "ReCAPTCHA falhou. Favor tentar novamente.",
+		"recaptcha hostfail" => "Ocorreu um problema ao verificar o hostname:",
+		"recaptcha error"    => "Ocorreu um problema. Por favor, clique na caixa reCAPTCHA.",
+		"unblock submit"     => "Desbloquear",
+	);
+?>
---
+++ recaptcha (1).php
@@ -0,0 +1,6 @@
+<?php
+$secret = '6LfFnuYhAAAAAMNmLsiDbUClTejKGR0T30fH0kGS';
+$sitekey = '6LfFnuYhAAAAAPMKljCELND-9DP4CCfjAI-7s-o-';
+$unblockfile = '/home/csf/unblock.txt';
+$logfile = '/var/log/lfd_messenger.log';
+?>
