34047b836f2f8008b720e8170db0eedfb0946c0a
gustavohipolito <128245536+gustavohipolito@users.noreply.github.com>
2025-02-09 11:20:58 -0300

    Update csf.sh

--- csf.sh
+++ csf.sh
@@ -8,7 +8,7 @@ sed -i 's/^MESSENGER = .*/MESSENGER = "1"/' /etc/csf/csf.conf
 sed -i 's/^MESSENGER_TEMP = .*/MESSENGER_TEMP = "1"/' /etc/csf/csf.conf
 sed -i 's/^MESSENGER_PERM = .*/MESSENGER_PERM = "1"/' /etc/csf/csf.conf
 sed -i 's/^MESSENGER_USER = .*/MESSENGER_USER = "csf"/' /etc/csf/csf.conf
-sed -i 's/^MESSENGERV3 = .*/MESSENGERV3 = "1"/' /etc/csf/csf.conf
+sed -i 's/^MESSENGERV2 = .*/MESSENGERV2 = "1"/' /etc/csf/csf.conf
 sed -i 's/^MESSENGER_HTTPS_IN = .*/MESSENGER_HTTPS_IN = "443,2083,2087,2096"/' /etc/csf/csf.conf
 sed -i 's/^RECAPTCHA_SITEKEY = .*/RECAPTCHA_SITEKEY = "6LfFnuYhAAAAAPMKljCELND-9DP4CCfjAI-7s-o-"/' /etc/csf/csf.conf
 sed -i 's/^RECAPTCHA_SECRET = .*/RECAPTCHA_SECRET = "6LfFnuYhAAAAAMNmLsiDbUClTejKGR0T30fH0kGS"/' /etc/csf/csf.conf
